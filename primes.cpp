095f3a6837610c2684800c12714abd060e3e59f6
Jett Thompson <jett.thompson.59@gmail.com>
2021-11-30 20:17:36 +1100

    made the prime finder multi threaded with an increasing backlog of primes (due to console writing being slower than calculating primes in parallel)

--- primes.cpp
+++ primes.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
 
 template<typename IntType>
 constexpr bool isDivisible(IntType dividend, IntType divisor) {
@@ -17,11 +21,59 @@ constexpr bool isPrime(IntType n) {
 
 using namespace std;
 
-int main (int argc, char** argv) {
-for (unsigned long long i = 2147483647ull;; i += 2) {
+class primeOutputBuffer {
+    private:
+     mutex theMutex;
+     queue<unsigned long long> theQueue;
+     public:
+      void push(unsigned long long value) {
+          unique_lock<mutex> lock(theMutex);
+          theQueue.push(value);
+          lock.unlock();
+       }
+       unsigned long long pop() {
+           unique_lock<mutex> lock(theMutex);
+           unsigned long long result = theQueue.back();
+           theQueue.pop();
+           lock.unlock();
+           return result;
+       }
+       int size() { return theQueue.size(); }
+};
+
+struct WorkerContext {
+    int start;
+  int increment;
+  int foundPrimes = 0;
+  primeOutputBuffer* outputBuffer;
+};
+
+int primeFinderWorker(WorkerContext* context) {
+    for (unsigned long long i = context->start;; i += context->increment) {
 if (isPrime(i)) {
-cout << i << endl;
+  context->outputBuffer->push(i);
+  context->foundPrimes++;
 }
 }
-return 0;
+}
+
+int main (int argc, char** argv) {
+  auto numWorkers = thread::hardware_concurrency();
+  WorkerContext contexts[numWorkers];
+  primeOutputBuffer outputBuffer;
+  for (int i = 0; i < numWorkers; i ++) {
+    WorkerContext context;
+    context.start = i + 2;
+    context.increment = numWorkers;
+    context.outputBuffer = &outputBuffer;
+    contexts[i] = context;
+    thread workerThread(primeFinderWorker, &contexts[i]);
+    workerThread.detach();
+  }
+  while (true) {
+      while (outputBuffer.size() < 1)
+        ;
+      cout << outputBuffer.pop() << " with " << outputBuffer.size() << "Remaining" << endl;
+  }
+  return 0;
 }
